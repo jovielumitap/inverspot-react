f04c1987df409221d9e0025133173bce5e19c056
jovielumitap <jovie.lumitap@gmail.com>
2019-08-13 22:06:39 +0800

    Updates

---
+++ precache-manifest.9db00f4389c9c694fe3e7c27eba3f2d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab22845c8964135aa8caa5d6bfe8759",
+    "url": "/inverspot-react/index.html"
+  },
+  {
+    "revision": "aea6bace42434036b960",
+    "url": "/inverspot-react/static/css/2.52787fb6.chunk.css"
+  },
+  {
+    "revision": "3de5f9d5143c8b2f3a9b",
+    "url": "/inverspot-react/static/css/main.7fef307a.chunk.css"
+  },
+  {
+    "revision": "aea6bace42434036b960",
+    "url": "/inverspot-react/static/js/2.499088a1.chunk.js"
+  },
+  {
+    "revision": "3de5f9d5143c8b2f3a9b",
+    "url": "/inverspot-react/static/js/main.b1fb4568.chunk.js"
+  },
+  {
+    "revision": "a05faaddf02567fdcc33",
+    "url": "/inverspot-react/static/js/runtime~main.9425e7e9.js"
+  }
+]);
